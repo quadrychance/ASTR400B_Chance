618aaaeb20323eaddcfb22ba035692c58ada307a
quadrychance <qchance85@gmail.com>
2018-03-29 19:51:52 -0400

    hm7

--- M33_Analytic.py
+++ M33_Analytic.py
@@ -10,49 +10,49 @@ class M33AnalyticOrbit:
         self.G = 4.498768e-6 #define constant
 
         # x, y, z postion of M33 from M31 frame
-        self.x = -98.
-        self.y = -120.
-        self.z = -127.
+        self.x = -98
+        self.y = -120
+        self.z = -127
         #vx, vy, vz of M33 from M31 frame
-        self.vx = -28.
-        self.vy = 174.
-        self.vz = 92.
+        self.vx = -28
+        self.vy = 174
+        self.vz = 92
 
         #M31 data
-        self.rd = 5.
+        self.rd = 5
         self.Mdisk = 0.12e12
-        self.rbulge = 1.
+        self.rbulge = 1
         self.Mbulge = 0.019e12
-        self.rhalo = 62.
+        self.rhalo = 62
         self.Mhalo = 1.921e12
 
     def HernquistAccel(self,M,r_a,x,y,z,acomp):  #input bulge+halo mass, scale length, positions, wanted accelerationcomponent
-        if acomp=='z':
+        if acomp == 'z':
             c = z
-        if acomp=='y':
+        if acomp == 'y':
             c = y
-        if acomp=='x':
+        if acomp == 'x':
             c = x
         r = np.sqrt(x**2+y**2+z**2)         #position vector
         a = -(self.G*M*c)/(r*(r_a+r)**2)    #hernquist acceleration
 
 
-        return a    #hernquist accel
+        return a
 
     def MiyamotoNagaiAccel(self,M,rd,x,y,z,acomp):
 
-        H = rd/5.           #disk scale height
+        H = rd/5           #disk scale height
         B = rd+np.sqrt(z**2+H**2)
         R = np.sqrt(x**2+y**2)
 
         if acomp == 'z':
-            a = -(self.G*M*B*z)/(((R**2+B**2)**1.5)*(np.sqrt(z**2+H**2)))
+            amna = -(self.G*M*B*z)/(((R**2+B**2)**1.5)*(np.sqrt(z**2+H**2)))
         if acomp == 'y':
-            a = -(self.G*M*y)/((R**2+B**2)**1.5)
+            amna = -(self.G*M*y)/((R**2+B**2)**1.5)
         if acomp == 'x':
-            a = -(self.G*M*x)/((R**2+B**2)**1.5)
+            amna = -(self.G*M*x)/((R**2+B**2)**1.5)
 
-            return a
+        return amna
 
     def M31Accel(self,x,y,z,acomp):
 
@@ -60,16 +60,16 @@ class M33AnalyticOrbit:
         a_h = self.HernquistAccel(self.Mhalo,self.rhalo,x,y,z,acomp)
         a_d = self.MiyamotoNagaiAccel(self.Mdisk,self.rd,x,y,z,acomp)
 
-        total = a_b+a_h+a_d
-        #Output: the sum of the accelerations and direction i
+        total = a_b + a_h + a_d
+
         return total
 
 
     def LeapFrog(self,dt,x,y,z,vx,vy,vz):
 
-        xhalf = x+vx*dt/2.
-        yhalf = y+vy*dt/2.
-        zhalf = z+vz*dt/2.
+        xhalf = x+vx*dt/2
+        yhalf = y+vy*dt/2
+        zhalf = z+vz*dt/2
 
 
         M31A_x = self.M31Accel(xhalf,yhalf,zhalf,'x')
@@ -100,14 +100,14 @@ class M33AnalyticOrbit:
 
 
         stepper = int((tf-ti)/dt)+1
-        Orbit = np.zeros((steps,7))
+        Orbit = np.zeros((stepper,7))
 
-        Orbit[0,1], Orbit[0,2], Orbit[0,3] = x,y,z
-        Orbit[0,4], Orbit[0,5], Orbit[0,6] = vx,vy,vz
+        Orbit[0,1], Orbit[0,2], Orbit[0,3] = x, y, z
+        Orbit[0,4], Orbit[0,5], Orbit[0,6] = vx, vy, vz
         Orbit[0,0] = ti
 
-        np.savetxt('M33Ana', Orbit, header='t   x    y    z   vx   vy   vz', comments='# ',
-                fmt=['%.2f', '%.2f','%.2f','%.2f','%.2f','%.2f','%.2f'])
+        #np.savetxt('M33Analytic', Orbit, header='t   x    y    z   vx   vy   vz', comments='# ',
+                #fmt=['%.2f', '%.2f','%.2f','%.2f','%.2f','%.2f','%.2f'])
         t=ti+dt
         n=1
         while t <= tf:
@@ -126,7 +126,7 @@ class M33AnalyticOrbit:
             vx,vy,vz = output[3], output[4], output[5]
 
         #Save results into text file
-        np.savetxt('M33Ana', Orbit, header='t   x    y    z   vx   vy   vz', comments='# ',
+        np.savetxt('M33Analytic', Orbit, header='t   x    y    z   vx   vy   vz', comments='# ',
                 fmt=['%.2f', '%.2f','%.2f','%.2f','%.2f','%.2f','%.2f'])
 
         return n
--- hw7plotter.py
+++ hw7plotter.py
@@ -1,26 +1,33 @@
 import matplotlib.pyplot as plt
+from M33_Analytic import M33AnalyticOrbit
+import numpy as np
 
-M31Orbit = np.genfromtxt('Orbit_M31.txt',dtype=None,names=True)
-M33Orbit = np.genfromtxt('Orbit_M33.txt',dtype=None,names=True)
-M33Analytic = np.genfromtxt('M33Ana',dtype=None,names=True)
+
+M31Orbit = np.genfromtxt('Orbits_M31.txt',names=True)
+M33Orbit = np.genfromtxt('Orbits_M33.txt',names=True)
+M33Analytic = np.genfromtxt('M33Analytic',names=True)
 
 
 sim_sep = np.sqrt((M33Orbit['x']-M31Orbit['x'])**2+(M33Orbit['y']-M31Orbit['y'])**2+(M33Orbit['z']-M31Orbit['z'])**2)
 
 a_sep = np.sqrt(M33Analytic['x']**2+M33Analytic['y']**2+M33Analytic['z']**2)
 
-
+plt.xkcd()
 plt.figure(1,figsize=(10,10))
-plt.plot(M31Orbit['t'], sim_sep, c= 'r' label='Sim')
-plt.plot(M33Analytic['t'], a_sep, c='b', label='Analytic')
+plt.plot(M31Orbit['t']/10, sim_sep, c= 'r', label ='Sim')  #had some weird timescale issue here
+plt.plot(M33Analytic['t'], a_sep, c='b', label ='Analytic')
 plt.title('Seperation of M33 and M31')
-plt.legend()
+plt.xlim(0,20)
 
+plt.legend()
+plt.show()
 
 sim_v = np.sqrt((M31Orbit['vx']-M33Orbit['vx'])**2+(M31Orbit['vy']-M33Orbit['vy'])**2+(M31Orbit['vz']-M33Orbit['vz'])**2)
 a_v = np.sqrt(M33Analytic['vx']**2+M33Analytic['vy']**2+M33Analytic['vz']**2)
 plt.figure(1,figsize=(10,10))
-plt.plot(M31Orbit['t'], sim_v, c= 'r' label='Sim')
-plt.plot(M33Analytic['t'], a_v, c='b', label='Analytic'))
+plt.plot(M31Orbit['t']/10, sim_v, c= 'r', label='Sim')
+plt.plot(M33Analytic['t'], a_v, c='b', label='Analytic')
 plt.title('Velocity Dispersion between M33 and M31')
 plt.legend()
+
+plt.show()
